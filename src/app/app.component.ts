8a6951175cb7680541718c0e8eb3cec27c78a96b
Umblum12 <abelardo_cruzleos@hotmail.com>
2024-05-14 11:16:56 -0700

    Proyecto Actualizado

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'] // Asegúrate de usar 'styleUrls' en lugar de 'styleUrl'
+})
+export class AppComponent {
+  title = 'Protafolio-Web';
+}
