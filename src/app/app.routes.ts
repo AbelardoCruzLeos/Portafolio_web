54c693b61f4c3da304c8ecf0a66551932b00fe6e
Umblum12 <abelardo_cruzleos@hotmail.com>
2024-06-07 10:26:21 -0700

    UpdateAppRutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { GamesComponent } from './pages/games/games.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Ruta para el componente Home
-  { path: 'games', component: GamesComponent }, // Ruta para el componente Home
+  { path: 'juegos', component: GamesComponent }, // Ruta para el componente games
+  { path: 'acercade', component: AcercadeComponent }, // Ruta para el componente acercade
   { path: '**', component: HomeComponent }, // Ruta para el componente Home
 ];
