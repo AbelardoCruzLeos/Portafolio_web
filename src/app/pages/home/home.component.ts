27f51419a38c7da58d70798f7699c4fdeb8d583e
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2024-06-24 16:07:34 -0700

    Eliminacion de los console.log

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,28 +14,12 @@ export class HomeComponent implements OnInit {
   seccionActiva: string = 'proyectos';
   proyectos: any[] = [];
   repos: any[] = [];
+  reposPorPage: any[][] = []; // Cambiado a matriz para paginación
   currentPage: number = 1;
   reposPerPage: number = 9;
   pagesArray: number[] = [];
   TotalRepos: number = 0;
   TotalPages: number = 0;
-
-  // Mapa de lenguajes a íconos y colores
-  lenguajeInfo: any = {
-    JavaScript: {
-      icon: 'https://img.icons8.com/color/48/000000/javascript.png',
-      color: '#f7df1e',
-    },
-    TypeScript: {
-      icon: 'https://img.icons8.com/color/48/000000/typescript.png',
-      color: '#3178c6',
-    },
-    Python: {
-      icon: 'https://img.icons8.com/color/48/000000/python.png',
-      color: '#3572A5',
-    },
-    // Agrega más lenguajes aquí según sea necesario
-  };
 
   constructor(
     private proyectosService: ProyectosService,
@@ -59,7 +43,6 @@ export class HomeComponent implements OnInit {
             Participantes: proyecto.Participantes,
             Lenguajes: proyecto.Lenguajes,
           }));
-          console.log(this.proyectos);
         },
         (error) => {
           console.error('Error al obtener proyectos:', error);
@@ -71,20 +54,24 @@ export class HomeComponent implements OnInit {
     this.githubService.getAllRepos().subscribe(
       (repos) => {
         this.repos = repos;
-        console.log(this.repos.length + 'Repositorios');
-        this.TotalRepos = this.repos.length;
-        console.log(this.TotalRepos + 'Total de repositorios');
+        this.TotalRepos = repos.length;
         this.TotalPages = Math.ceil(this.TotalRepos / this.reposPerPage);
-        console.log(this.TotalPages + 'Total de paginas');
+        this.pagesArray = this.getPagesArray();
 
-        for (let i = 0; i < repos.length; i += this.reposPerPage) {
-          this.repos.push(repos.slice(i, i + this.reposPerPage));
-        }
-      },
+        this.reposPorPage = this.divideArrayInChunks(repos, this.reposPerPage);
+      }, 
       (error) => {
         console.error('Error al obtener repositorios:', error);
       }
     );
+  }
+
+  divideArrayInChunks(array: any[], chunkSize: number): any[][] {
+    const result: any[][] = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
   }
 
   cambiarSeccion(seccion: string): void {
@@ -105,19 +92,17 @@ export class HomeComponent implements OnInit {
       return; // Evitar cargar páginas inválidas
     }
     this.currentPage = pageNumber;
-    this.cargarRepositorios();
   }
 
   getPagesArray(): number[] {
-    const totalPages = Math.ceil(this.TotalRepos / 9);
-    return Array(totalPages)
+    return Array(this.TotalPages)
       .fill(0)
-      .map((x, i) => i + 1);
+      .map((_, i) => i + 1);
   }
 
   getLanguageIconUrl(language: string): string {
     switch (language.toLowerCase()) {
-      case 'c#':
+      case 'c#': 
         return 'https://cdn3d.iconscout.com/3d/free/thumb/free-c-sharp-9294854-7577997.png?f=webp';
       case 'llvm':
         return 'https://e7.pngegg.com/pngimages/528/38/png-clipart-clang-llvm-gnu-compiler-collection-c-llvm-c-google-chrome.png';
@@ -125,7 +110,6 @@ export class HomeComponent implements OnInit {
         return 'https://cdn3d.iconscout.com/3d/free/thumb/free-javascript-9294848-7577991.png?f=webp';
       case 'typescript':
         return 'https://cdn3d.iconscout.com/3d/free/thumb/free-typescript-9294849-7577992.png';
-      // Reemplaza 'URL_DEL_ICONO_C_PLUS_PLUS' con la URL real del icono de C++
       default:
         return 'URL_DEL_ICONO_POR_DEFECTO'; // URL de un icono por defecto si el lenguaje no tiene un icono específico
     }
