27434754860f54eb1224783b54c8a6efd2e01b4c
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2024-06-24 17:45:07 -0700

    Actualizacion de repositorios 2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
 
   cargarProyectos(): void {
     this.proyectosService
-      .getProyects('https://apiportafolioweb-production.up.railway.app//proyectos', { page: 0, perPage: 10 })
+      .getProyects('https://apiportafolioweb-production.up.railway.app/proyectos', { page: 0, perPage: 10 })
       .subscribe(
         (proyectos: any) => {
           this.proyectos = proyectos.map((proyecto: any) => ({
